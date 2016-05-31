82a859bf99a3b49264fbd6b06be47ce4434291ef
Francis Kim <francis.kim@yale.edu>
2016-05-31 17:05:44 -0400

    Cosmetic changes for Heroku deployment

--- frontend/cruddy_reads.jsx
+++ frontend/cruddy_reads.jsx
@@ -5,6 +5,8 @@ var ReactDOM = require("react-dom")
 var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
+var hashHistory = ReactRouter.hashHistory;
 
 var App = React.createClass({
   render: function() {
@@ -19,10 +21,12 @@ var App = React.createClass({
 });
 
 var Router = (
-  <Router
+  <Router history={hashHistory}>
+    <Route path="/" component={App}/>
+  </Router>
 );
 
 
 document.addEventListener("DOMContentLoaded", function() {
-  ReactDOM.render(<p>HEY</p>, document.getElementById("content"));
+  ReactDOM.render(Router, document.getElementById("content"));
 })
