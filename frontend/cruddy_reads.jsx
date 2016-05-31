d4594e048cddd681c91e2973a59b9175a3b924b8
Francis Kim <francis.kim@yale.edu>
2016-05-31 16:36:31 -0400

    Began work on backend auth and prep for Heroku

--- frontend/cruddy_reads.jsx
+++ frontend/cruddy_reads.jsx
@@ -2,15 +2,25 @@
 var React = require("react")
 var ReactDOM = require("react-dom")
 
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+
 var App = React.createClass({
   render: function() {
     return(
       <div>
         <header><h1>CRUDDY READS</h1></header>
+        <LoginForm/>
+        {this.props.children}
       </div>
     );
   }
 });
+
+var Router = (
+  <Router
+);
 
 
 document.addEventListener("DOMContentLoaded", function() {
