48a4fc98ffac83ef4a39a14d652fd06a3733e7fd
Francis Kim <francis.kim@yale.edu>
2016-06-09 09:43:04 -0400

    added es2015 to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel',
         query: {
-          presets: ['react']
+          presets: ['react', 'es2015']
         }
       }
     ]
